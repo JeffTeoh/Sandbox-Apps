ed28e0f618617d94764aff60bbec567b6b723ba4
JeffTeoh <jeffreyteoh4311@gmail.com>
2018-02-20 12:50:44 +0800

    Revert "Testing configuration of FB Login"
    This reverts commit 4b2fe69b2f3a1a6c20dd84d438196ad8249c51e2.

--- app/src/main/java/com/example/android/sandboxapps/MainActivity.java
+++ app/src/main/java/com/example/android/sandboxapps/MainActivity.java
@@ -5,53 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import com.facebook.FacebookSdk;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView info;
-    private LoginButton loginButton;
-    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        FacebookSdk.sdkInitialize(getApplicationContext());
-        callbackManager = CallbackManager.Factory.create();
-
-        info = (TextView)findViewById(R.id.info);
-        loginButton = (LoginButton)findViewById(R.id.login_button);
-    }
-
-    loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-        @Override
-        public void onSuccess(LoginResult loginResult) {
-            info.setText(
-                    "User ID: "
-                            + loginResult.getAccessToken().getUserId()
-                            + "\n" +
-                            "Auth Token: "
-                            + loginResult.getAccessToken().getToken()
-            );
-        }
-
-        @Override
-        public void onCancel() {
-            info.setText("Login attempt canceled.");
-        }
-
-        @Override
-        public void onError(FacebookException e) {
-            info.setText("Login attempt failed.");
-        }
-    });
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     public void aboutUs(View view) {
